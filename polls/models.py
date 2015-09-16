3e831138af4fe5046703a21dc359ae4e16d1dbf5
Nick Rutigliano <rutigliano.nick@gmail.com>
2015-09-16 16:50:47 -0700

    updated __unicode__ for voter model

--- polls/models.py
+++ polls/models.py
@@ -40,4 +40,4 @@ class Voter(models.Model):
     poll = models.ForeignKey(Poll)
 
     def __unicode__(self):
-        return self.user.name + " voted on " + self.poll.name
+        return self.user
