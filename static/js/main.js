6b6ffd54810ae16e63f9d2fee49f76e4a0f392d3
Benson Li <bl4074@hotmail.com>
2015-12-05 01:44:22 -0800

    Implemented player -> visualization next

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var currentIsOpen = true;
     var pastIsOpen = true;
+    var myPlayer = videojs('test1'); 
     
     $("#current_polls_arrow").click(function(){
         togglePollContainers($(this), currentIsOpen);
@@ -13,20 +14,76 @@ $(document).ready(function(){
     });
     
     $(".play").click(function(){
-        $(this).toggleClass("pause");        
+        $(this).toggleClass("icon-pause-circle");
+        
+        var message = $(this).next(".play_message").text();
+        if(message == "PLAY") {
+            $(this).next(".play_message").text("PAUSE");
+            myPlayer.play();
+        }
+        else {
+            $(this).next(".play_message").text("PLAY");
+            myPlayer.pause();
+        }
     });
     
-    $(".play").mouseenter(function(){
+    $(".icon-play-circle").mouseenter(function(){
         var message = "";
-        if($(this).hasClass("pause"))
+        if($(this).hasClass("icon-pause-circle"))
             message = "PAUSE";
         else
             message = "PLAY";
+    
         $(this).after("<h3 class='play_message'>" + message + "</h3>");
-        $(".play").mouseleave(function(){
+        $(".icon-play-circle").mouseleave(function(){
             $(this).next(".play_message").remove();
         });
     });
+    
+    $(".icon-skipbackward").mouseenter(function(){
+        $(this).addClass("icon-skipbackward-fill");
+        $(this).mouseleave(function(){
+            $(this).removeClass("icon-skipbackward-fill");
+        });
+    });
+    
+    $(".icon-skipforward").mouseenter(function(){
+        $(this).addClass("icon-skipforward-fill");
+        $(this).mouseleave(function(){
+            $(this).removeClass("icon-skipforward-fill");
+        });
+    });
+    
+    $("#center_button").click(function(){
+        var $target = $(this);
+        $(this).toggleClass("icon-pause");
+        if($target.hasClass("icon-pause-fill")) {
+            $(this).removeClass("icon-pause-fill").addClass("icon-play-fill");
+            myPlayer.pause();
+        }
+        else
+            myPlayer.play();
+        
+            
+        togglePlayButtonState($target);
+    });
+    
+    $("#center_button").mouseenter(function(){
+        togglePlayButtonState($(this));
+    });
+    
+    function togglePlayButtonState($target){
+        var icon = "";
+        if($target.hasClass("icon-pause"))
+            icon = "pause";
+        else {
+            icon = "play";
+        }
+        $target.addClass("icon-" + icon + "-fill");
+        $target.mouseleave(function(){
+            $target.removeClass("icon-" + icon + "-fill");
+        });        
+    }
     
     function togglePollContainers($target, poll_state) {
         if(poll_state == true) {
@@ -40,6 +97,13 @@ $(document).ready(function(){
         $target.toggleClass("open_poll_cont");
     }
     
+    /*
+        https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API
+        https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API#Creating_a_waveformoscilloscope
+        https://github.com/mdn/voice-change-o-matic/blob/gh-pages/scripts/app.js#L123-L167
+    
+    
+    */
     
     /*// stack overflow homies - question ?
     (function() {
